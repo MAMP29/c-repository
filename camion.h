abb2e5ddcd5502299c362cc9be8d8b035ccc534a
Miguel200434 <106500061+Miguel200434@users.noreply.github.com>
2023-04-22 14:33:53 -0500

    Create camion.h

---
+++ camion.h
@@ -0,0 +1,50 @@
+#ifndef CAMION_H
+#define CAMION_H
+#include <string>
+#include <vector>
+using namespace std;
+
+
+class Camion
+{
+  private:
+  string placaCamion;
+  int capacidadMaxima;
+  int pesoTotalCargado;
+  vector<int> objetos;
+
+  public:
+  /**
+  Constructor. Variables iniciales a atributos internos.
+  */
+  Camion(string laPlaca, int laCapacidadMaxima);
+  /**
+  Evalua si puede cargar un objeto o no. Retorna 0 = false (No se puede hacer) y 1 = true (Se puede hacer).
+  */
+  bool cargarUnObjeto(int pesoDelObjeto);
+  /**
+  Hacer la mudanza. Retorna la carga que transporto.
+  */
+  int hacerMudanza();
+  /**
+  Saber cual es la placa y capacidad maxima del camion y el peso total cargado.
+  */
+  string cualEsLaPlaca();
+  int cualEsLaCapacidadMaxima();
+  int cualEsElPesoTotalCargado();
+
+  /**
+   Mover los objetos que se puedan a otro camión. Debe quitar
+   los elementos del vector de objetos y actualizar la variable pesoTotalCargado
+  */
+  void descargarACamion(Camion &Cv, Camion &Cn);
+  //descarga un objeto especificado
+  void descargarUnObjeto(int objeto);
+//recordar actualizar el peso total cargado
+
+//devuelve los objetos cargados
+  vector <int> getObjetos();
+
+};
+
+#endif 
