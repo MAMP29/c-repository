29fc711d38afac394e831b794f951d97506b8441
Miguel200434 <106500061+Miguel200434@users.noreply.github.com>
2023-04-22 14:35:16 -0500

    Create camion.cpp

---
+++ camion.cpp
@@ -0,0 +1,55 @@
+#include "camion.h"
+#include "EmpresaDeMudanza.h"
+#include <iostream>
+#include <algorithm>
+Camion::Camion(string laPlaca, int laCapacidadMaxima)
+{
+  placaCamion = laPlaca;
+  capacidadMaxima = laCapacidadMaxima;
+  pesoTotalCargado = 0;
+}
+
+bool Camion::cargarUnObjeto(int pesoDelObjeto)
+{
+  if(pesoTotalCargado + pesoDelObjeto <= capacidadMaxima)
+  {
+    objetos.push_back(pesoDelObjeto);
+    pesoTotalCargado += pesoDelObjeto;
+    return true;
+  }
+  else
+    return false;
+}
+
+int Camion::hacerMudanza()
+{
+  return pesoTotalCargado;
+  objetos.clear();
+}
+
+string Camion::cualEsLaPlaca()
+{
+  return placaCamion;
+}
+int Camion::cualEsLaCapacidadMaxima()
+{
+  return capacidadMaxima;
+}
+int Camion::cualEsElPesoTotalCargado(){
+  return pesoTotalCargado;
+}
+vector <int> Camion::getObjetos(){
+  return objetos;
+}
+void Camion::descargarUnObjeto(int objetoElim)
+{
+  for(int i; i < objetos.size(); i++){
+    if(objetos.at(i)==objetoElim){
+      objetos.erase(objetos.begin() + i);
+      pesoTotalCargado -= objetoElim;
+      std::cout<<"Objeto descargado"<<std::endl;
+    }else{
+      std::cout<<"Objeto "<<objetoElim<<" No encontrado "<<std::endl;  
+    }
+  }
+}
